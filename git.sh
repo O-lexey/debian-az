253d612d7df4c8555ecf87e8436ba097656b06a3
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2017-08-07 00:16:40 +0300

    Corrected git global config for my email

--- git.sh
+++ git.sh
@@ -2,4 +2,4 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y git
 git config --global user.name "Aliaksei Zhukau"
-git config --global user.name zhukau.aliaksei@gmail.com
+git config --global user.email zhukau.aliaksei@gmail.com
