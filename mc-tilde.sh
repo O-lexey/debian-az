1f791965365c396e4787879e7ef2e020f0eb82e3
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-13 20:23:46 +0300

    now install from official repos

--- mc-tilde.sh
+++ mc-tilde.sh
@@ -1,6 +1,2 @@
-cp etc/apt/sources.list.d/tilde.list /etc/apt/sources.list.d/tilde.list
-apt-get update
-apt-get install -y tilde-keyring
-apt-get update
 apt-get install -y tilde mc
 echo 'SELECTED_EDITOR="/usr/bin/tilde"' > ~/.selected_editor
