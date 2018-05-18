77c20df5b1ea6788b638947d27903295f1f93943
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-18 21:24:17 +0300

    Dart-Dev

--- dart-dev.sh
+++ dart-dev.sh
@@ -1,2 +1,7 @@
 apt-get update
 apt-get install apt-transport-https
+sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list > /etc/apt/sources.list.d/dart_unstable.list'
+apt-get update
+apt-get install dart
